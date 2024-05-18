08fb4ea824769dc007b3212ea6ea83c1f287fdd6
mehdi39 <jahid4k@gmail.com>
2024-05-18 14:41:03 +0600

    theme: working on theme config

---
+++ theme.ts
@@ -0,0 +1,9 @@
+export const decor_colors = {
+  White: '#ffffff',
+  Black: '#363130',
+  Salmon: '#f4b5a4',
+  Terracotta: '#cc7861',
+  BrownRosy: '#dcbeb6',
+  Beige: '#faf0e6',
+  OrangeDark: '#cc7861',
+};
